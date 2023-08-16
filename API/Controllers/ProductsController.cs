5f6180b17c3103a4cce618f40baa6b286335435e
Samuel Zaire Rocha <96780102+szaire@users.noreply.github.com>
2023-08-16 09:13:22 -0300

    update files

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using API.Contexts;
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -8,26 +9,29 @@ namespace API.Controllers
 	[Route("api/[controller]")]
 	public class ProductsController : ControllerBase
 	{
-		// Criando a variável de acesso ao contexto (banco de dados) StoreContext
-		private readonly StoreContext context;
+		// Criando a variável de acesso ao contexto (banco de dados) StoreContext por meio de injeção de dependência
+        private readonly StoreContext _context;
 
 		public ProductsController(StoreContext context)
 		{
-			this.context = context;
+            _context = context;
 		}
 
+		// Implementando métodos assincronos:
+		// async na assinatura do método
+		// Task<retorno do método>
+		// await (no método de acesso ao banco de dados)
 		[HttpGet]
-		public ActionResult<List<Product>> GetProducts()
+		public async Task<ActionResult<List<Product>>> GetProducts()
 		{
-			var products = context.Products.ToList();
+			var products = await _context.Products.ToListAsync();
 			return Ok(products);
 		}
 
 		[HttpGet("{id}")]
-		public ActionResult<Product> GetProduct(int id)
+		public async Task<ActionResult<Product>> GetProduct(int id)
 		{
-			var product = context.Products.Find(id);
-
+			var product = await _context.Products.FindAsync(id);
 			return Ok(product);
 		}
 	}

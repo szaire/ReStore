1249fb6e4d7101fd597facc14ba4270fdb3565aa
Samuel Zaire Rocha <96780102+szaire@users.noreply.github.com>
2023-08-16 10:57:51 -0300

    added frontend

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,17 +10,23 @@ namespace API.Controllers
 	public class ProductsController : ControllerBase
 	{
 		// Criando a variável de acesso ao contexto (banco de dados) StoreContext por meio de injeção de dependência
-        private readonly StoreContext _context;
+		private readonly StoreContext _context;
 
 		public ProductsController(StoreContext context)
 		{
-            _context = context;
+			_context = context;
 		}
 
 		// Implementando métodos assincronos:
 		// async na assinatura do método
 		// Task<retorno do método>
 		// await (no método de acesso ao banco de dados)
+
+		// Condições para se usar assincronicidade
+		// - Quando a experiência do usuário é a principal preocupação. Ou seja, permitir que o usuário
+		// realize diversas tarefas ao mesmo tempo e todo o resto esteja sendo processado por debaixo
+		// dos panos.
+
 		[HttpGet]
 		public async Task<ActionResult<List<Product>>> GetProducts()
 		{
--- API/Program.cs
+++ API/Program.cs
@@ -2,46 +2,53 @@ using API.Contexts;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<StoreContext>(options => {
-	options.UseSqlite(builder.Configuration.GetConnectionString("DevelopmentConnection"));
-});
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+
+		builder.Services.AddControllers();
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+
+		builder.Services.AddDbContext<StoreContext>(options =>
+		{
+			options.UseSqlite(builder.Configuration.GetConnectionString("DevelopmentConnection"));
+		});
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+
+		app.MapControllers();
+
+		var scope = app.Services.CreateScope();
+		var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+		var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+		try
+		{
+			context.Database.Migrate();
+			DbInitializer.Init(context);
+		}
+		catch (Exception ex)
+		{
+			logger.LogError(ex, "A problem occurred during migration process.");
+		}
+
+		app.Run();
+	}
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-var scope = app.Services.CreateScope();
-var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
-var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-
-try
-{
-	context.Database.Migrate();
-	DbInitializer.Init(context);
-}
-catch (Exception ex)
-{
-	logger.LogError(ex, "A problem occurred during migration process.");
-}
-
-app.Run();
